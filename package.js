b70fac7c390df0a4ab86c5a62e82dbd3ca4f2ecd
Steve <oohaysmlm@gmail.com>
2016-05-20 16:52:04 -0700

    Version increment

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'skehoe1989:roles-and-groups-manager',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Flexible, secure UI for alanning:roles to manage group/role membership.',
   // URL to the Git repository containing the source code for this package.
